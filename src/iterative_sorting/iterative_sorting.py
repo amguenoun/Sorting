e451d450c4d5801f32d35db49e47d1518c979563
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-16 16:23:24 -0400

    implemented count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,7 @@
 # TO-DO: Complete the selection_sort() function below
+import random
+
+
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr)):
@@ -38,4 +41,28 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
 
-    return arr
+    if len(arr) in (0, 1):
+        return arr
+
+    for i in range(len(arr)):
+        if maximum < arr[i]:
+            maximum = arr[i]
+        elif arr[i] < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+
+    counts = [0] * (maximum + 1)
+    newArr = [0] * len(arr)
+
+    # Store the count of each unique number in arr
+    for i in range(len(arr)):
+        counts[arr[i]] += 1
+
+    # Modify count array to store the sum of previous counts
+    for i in range(1, len(counts)):
+        counts[i] = counts[i] + counts[i-1]
+
+    for i in range(len(arr)):
+        newArr[counts[arr[i]] - 1] = arr[i]
+        counts[arr[i]] -= 1
+
+    return newArr
